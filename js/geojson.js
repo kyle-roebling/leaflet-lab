d3e13a091b11a5a1b164954378f6b172d70a2153
Kyle Roebling <roebling@wisc.edu>
2020-01-31 16:02:27 -0600

    First Commit

---
+++ js/geojson.js
@@ -0,0 +1,69 @@
+
+// function to create map instance
+
+function createMap (){
+
+// create map object
+ var map = L.map('mapid', {
+     center: [20,0],
+     zoom: 2
+ }); 
+    
+// load tile layer
+ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+    
+// call getData
+getData(map);
+ 
+}
+
+// add popups to features
+function onEachFeature(feature, layer) {
+    //no property named popupContent; instead, create html string with all properties
+    var popupContent = "";
+    if (feature.properties) {
+        //loop to add feature property names and values to html string
+        for (var property in feature.properties){
+            popupContent += "<p>" + property + ": " + feature.properties[property] + "</p>";
+        }
+        layer.bindPopup(popupContent);
+    };
+};
+
+
+
+
+
+//function to retrieve the data and place it on the map
+function getData(map){
+     // load the data
+     $.ajax("data/MegaCities.geojson", {
+        dataType: "json",
+        success: function(response){
+              //create marker options
+            var geojsonMarkerOptions = {
+                radius: 8,
+                fillColor: "#ff7800",
+                color: "#000",
+                weight: 1,
+                opacity: 1,
+                fillOpacity: 0.8
+            };
+            
+             //create a Leaflet GeoJSON layer and add it to the map
+            L.geoJson(response, {
+                filter: function(feature){
+                   return feature.properties.Pop_2015 > 20; 
+                },
+                onEachFeature: onEachFeature,
+                pointToLayer: function (feature, latlng){
+                    return L.circleMarker(latlng, geojsonMarkerOptions);
+                }
+            }).addTo(map);
+        }
+    });
+};
+ 
+$(document).ready(createMap);
---
+++ js/main.js
@@ -0,0 +1,196 @@
+var freeBus = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "LineString",
+                "coordinates": [
+                    [-105.00341892242432, 39.75383843460583],
+                    [-105.0008225440979, 39.751891803969535]
+                ]
+            },
+            "properties": {
+                "popupContent": "This is a free bus line that will take you across downtown.",
+                "underConstruction": false
+            },
+            "id": 1
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "LineString",
+                "coordinates": [
+                    [-105.0008225440979, 39.751891803969535],
+                    [-104.99820470809937, 39.74979664004068]
+                ]
+            },
+            "properties": {
+                "popupContent": "This is a free bus line that will take you across downtown.",
+                "underConstruction": true
+            },
+            "id": 2
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "LineString",
+                "coordinates": [
+                    [-104.99820470809937, 39.74979664004068],
+                    [-104.98689651489258, 39.741052354709055]
+                ]
+            },
+            "properties": {
+                "popupContent": "This is a free bus line that will take you across downtown.",
+                "underConstruction": false
+            },
+            "id": 3
+        }
+    ]
+};
+
+var myStyle = {
+    "color": "red",
+    "weight": 5,
+    "opacity": 0.65
+};
+
+var bicycleRental = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -104.9998241,
+                    39.7471494
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "popupContent": "This is a B-Cycle Station. Come pick up a bike and pay by the hour. What a deal!"
+            },
+            "id": 51
+        },
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -104.9983545,
+                    39.7502833
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "popupContent": "This is a B-Cycle Station. Come pick up a bike and pay by the hour. What a deal!"
+            },
+            "id": 52
+        },
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -104.9963919,
+                    39.7444271
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "popupContent": "This is a B-Cycle Station. Come pick up a bike and pay by the hour. What a deal!"
+            },
+            "id": 54
+        },
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -104.9960754,
+                    39.7498956
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "popupContent": "This is a B-Cycle Station. Come pick up a bike and pay by the hour. What a deal!"
+            },
+            "id": 55
+        },
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -104.9933717,
+                    39.7477264
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "popupContent": "This is a B-Cycle Station. Come pick up a bike and pay by the hour. What a deal!"
+            },
+            "id": 57
+        },
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -104.9913392,
+                    39.7432392
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "popupContent": "This is a B-Cycle Station. Come pick up a bike and pay by the hour. What a deal!"
+            },
+            "id": 58
+        },
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -104.9788452,
+                    39.6933755
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "popupContent": "This is a B-Cycle Station. Come pick up a bike and pay by the hour. What a deal!"
+            },
+            "id": 74
+        }
+    ]
+};
+
+var geojsonMarkerOptions = {
+    radius: 8,
+    fillColor: "#ff7800",
+    color: "#000",
+    weight: 1,
+    opacity: 1,
+    fillOpacity: 0.8
+};
+
+var mymap = L.map('mapid').setView([39.75621, -104.99404], 13);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    accessToken: 'pk.eyJ1Ijoia3JvZWJsaW5nIiwiYSI6ImNqeXczaGplMjB3YjgzYmxyZGU1OG90bXUifQ.ItIrq8YGHvZIilkcx-U8Ag'
+}).addTo(mymap);
+
+function onEachFeature(feature, layer) {
+    // does this feature have a property named popupContent?
+    if (feature.properties && feature.properties.popupContent) {
+        layer.bindPopup(feature.properties.popupContent);
+    }
+}
+
+L.geoJSON(freeBus,{
+    style: myStyle,
+    onEachFeature: onEachFeature
+}).addTo(mymap);
+
+L.geoJSON(bicycleRental,{
+    pointToLayer: function(feature, latlng){
+        return L.circleMarker(latlng, geojsonMarkerOptions)
+            }
+}).addTo(mymap);
